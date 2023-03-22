846cfcdd258edc009451fa067041a1df916eca1a
niinasinani <blom.niina@gmail.com>
2023-03-22 21:08:49 +0200

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'd5bc06265dc539e7f4df3553027d4425', 
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
